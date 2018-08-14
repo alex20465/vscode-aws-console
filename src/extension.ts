021bf2358c07990efdb16290734e3eaf8f26a144
Alexandros Fotiadis <fotiadis90@gmail.com>
2018-08-14 04:19:10 +0300

    use awsconsole.ec2.region configuration

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,15 @@ import { EC2 } from "aws-sdk";
 import EC2Manager from "./managers/EC2Manager";
 
 export function activate(context: vscode.ExtensionContext) {
-    const ec2Client = new EC2({ region: "eu-central-1" });
+    const { region } = vscode.workspace.getConfiguration(
+        "awsconsole.ec2",
+        null
+    );
+
+    const ec2Client = new EC2({ region });
+
     const ec2Manager = new EC2Manager(ec2Client);
+
     ec2Manager.register();
 }
 
